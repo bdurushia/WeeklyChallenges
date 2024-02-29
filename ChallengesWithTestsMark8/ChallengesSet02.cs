f43c233401a69707844c79b39eaf72dd00d36315
Bryan Durushia <b.durushia@gmail.com>
2024-02-28 22:30:55 -0600

    Completed the Weekly Challenge Set 02. All tests passed.

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,125 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+            string alphabetUpper = alphabetLower.ToUpper();
+
+            if (alphabetLower.Contains(c) || alphabetUpper.Contains(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+
+            return vals.Length % 2 == 0 ? true : false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0 ? true : false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0 ? true : false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else if (numbers.Count() > 0)
+            {
+                return numbers.Min() + numbers.Max();
+            }
+            else
+            {
+                return 0;
+            }
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length < str2.Length ? str1.Length : str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return 0; 
+            }
+            else
+            {
+                int sum = 0;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                return sum;
+            }
+            
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return 0; 
+            }
+            else
+            {
+                int sum = 0;
+                foreach (int num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        sum += num;
+                    }
+                }
+                return sum;
+            }
+            
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return false; 
+            }
+
+            int sum = 0;
+
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum % 2 != 0 ? true : false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 1)
+            {
+                return 0;
+            }
+            else
+            {
+                long countOdds = number / 2;
+
+                return int.Parse(countOdds.ToString());
+            }
         }
     }
 }
